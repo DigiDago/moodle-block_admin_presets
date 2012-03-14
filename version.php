d6b7aaa78844595b7b21fdf763344b09f9a1c61e
David Monllaó <david.monllao@gmail.com>
2012-03-14 14:18:35 +0100

    CONTRIB-3522 - Changing ->requires

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 $plugin->version = 2012031401;
 $plugin->release = '1.0(Build: 2012031401)';
 
-$plugin->requires = 2010091500;
+$plugin->requires = 2010112400;
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
