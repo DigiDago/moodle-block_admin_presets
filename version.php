1e1a1f617243cf442cf9e711a2000b45a3dbb8e4
David Monllaó <david.monllao@gmail.com>
2012-03-28 12:51:00 +0200

    CONTRIB-3558 - Changing version

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-$plugin->version = 2012031401;
-$plugin->release = '1.0(Build: 2012031401)';
+$plugin->version = 2012032800;
+$plugin->release = '1.0(Build: 2012032800)';
 
 $plugin->requires = 2010112400;
 $plugin->cron     = 0;
