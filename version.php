5f36a746f772340f40bffb40c5c7390383a0c637
David Monllao <davidm@moodle.com>
2014-07-22 16:01:05 +0800

    Bumping to the first 2.7 version

--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014072200;
-$plugin->requires = 2013110500;
+$plugin->version = 2014072201;
+$plugin->requires = 2014050800;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '26.1 (Build: 2014072200)';
+$plugin->release = '27.1 (Build: 2014072201)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
