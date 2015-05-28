41650520b56df05baa6624315b700d69ac7ab45e
David Monllao <davidm@moodle.com>
2015-05-28 12:52:35 +0800

    Release for Moodle 2.8

--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014072201;
-$plugin->requires = 2014050800;        // Requires this Moodle version
+$plugin->version = 2014110400;
+$plugin->requires = 2014110400;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '27.1 (Build: 2014072201)';
+$plugin->release = '1.28.0 (Build: 2014110400)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
