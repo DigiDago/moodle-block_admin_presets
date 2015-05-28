8eb72083ab1c15e25666ad6dc59995cee5317843
David Monllao <davidm@moodle.com>
2015-05-28 13:08:17 +0800

    Release for Moodle 2.9

--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014110400;
-$plugin->requires = 2014110400;        // Requires this Moodle version
+$plugin->version = 2015050500;
+$plugin->requires = 2015050500;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '1.28.0 (Build: 2014110400)';
+$plugin->release = '1.29.0 (Build: 2015050500)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
