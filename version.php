4b2a7d097864ecf5cdc4eebcdd5e7f23c4bc4aa3
David Monllao <davidm@moodle.com>
2016-08-25 13:58:18 +0800

    Release for Moodle 3.0

--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015050500;
-$plugin->requires = 2015050500;        // Requires this Moodle version
+$plugin->version = 2015111600;
+$plugin->requires = 2015111600;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '1.29.0 (Build: 2015050500)';
+$plugin->release = '1.30.0 (Build: 2015111600)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
