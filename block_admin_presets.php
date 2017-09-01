c2913824d2e23d85ef133c41b88750127b908613
keokod <keo.n@free.fr>
2017-09-01 13:12:14 +0000

    update to  moodle 3.3 use  pix_url to image_url closes #7 change version 1.3 to 1.4

--- block_admin_presets.php
+++ block_admin_presets.php
@@ -27,15 +27,15 @@ class block_admin_presets extends block_list {
             return $this->content;
         }
 
-        $this->content->items[] = '<img src="'.$OUTPUT->pix_url("i/backup").'" class="icon" alt="'.get_string('actionexport', 'block_admin_presets').'" />
+        $this->content->items[] = '<img src="'.$OUTPUT->image_url("i/backup").'" class="icon" alt="'.get_string('actionexport', 'block_admin_presets').'" />
                                    <a title="'.get_string('actionexport', 'block_admin_presets').'" href="'.$CFG->wwwroot.'/blocks/admin_presets/index.php?action=export">
                                    '.get_string('actionexport', 'block_admin_presets').'</a>';
 
-        $this->content->items[] = '<img src="'.$OUTPUT->pix_url("i/restore").'" class="icon" alt="'.get_string('actionimport', 'block_admin_presets').'" />
+        $this->content->items[] = '<img src="'.$OUTPUT->image_url("i/restore").'" class="icon" alt="'.get_string('actionimport', 'block_admin_presets').'" />
                                    <a title="'.get_string('actionimport', 'block_admin_presets').'" href="'.$CFG->wwwroot.'/blocks/admin_presets/index.php?action=import">
                                    '.get_string('actionimport', 'block_admin_presets').'</a>';
 
-        $this->content->items[] = '<img src="'.$OUTPUT->pix_url("i/repository").'" class="icon" alt="'.get_string('actionbase', 'block_admin_presets').'" />
+        $this->content->items[] = '<img src="'.$OUTPUT->image_url("i/repository").'" class="icon" alt="'.get_string('actionbase', 'block_admin_presets').'" />
                                    <a title="'.get_string('actionbase', 'block_admin_presets').'" href="'.$CFG->wwwroot.'/blocks/admin_presets/index.php">
                                    '.get_string('actionbase', 'block_admin_presets').'</a>';
 
--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016052300;
+$plugin->version = 2017090100;
 $plugin->requires = 2016052300;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '1.31.0 (Build: 2016052300)';
+$plugin->release = '1.4.0 (Build: 2017090100)';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
