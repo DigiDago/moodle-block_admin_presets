afa1cc2687651465a3f172ca4de5b164314d9e28
kecherou <j.kesraoui@digidago.com>
2017-10-30 16:04:29 +0100

    Modification of travis file to test plugin for moodle 3.4

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017090100;
-$plugin->requires = 2017102500;        // Requires this Moodle version
+$plugin->version = 2017103000;
+$plugin->requires = 2016052300;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
-$plugin->release = '1.32.0 (Build: 2017090100)';
+$plugin->release = '2.0';
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
