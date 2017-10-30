39154b2e388ac485a40884d6bfaa0567b7ac6cb6
kecherou <j.kesraoui@digidago.com>
2017-10-30 15:52:59 +0100

    Version of Moodle changed in requires

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2017090100;
-$plugin->requires = 2016052300;        // Requires this Moodle version
+$plugin->requires = 2017102500;        // Requires this Moodle version
 $plugin->component = 'block_admin_presets';
 $plugin->release = '1.32.0 (Build: 2017090100)';
 $plugin->cron     = 0;
